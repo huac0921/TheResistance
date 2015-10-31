eb207fbdbaafaa95b93f1cd9bfd0be2ce3ce10a3
huac0921 <729666768@qq.com>
2015-10-31 12:55:05 +0800

    Initial Commit

---
+++ TheResistance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheResistance
+//
+//  Created by 华晨 on 15/10/31.
+//  Copyright © 2015年 ACH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TheResistance/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TheResistance
+//
+//  Created by 华晨 on 15/10/31.
+//  Copyright © 2015年 ACH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
